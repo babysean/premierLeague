3d897564bece657b2fed91d1d97254fe74414e21
babysean <tkdgns7070@gmail.com>
2017-07-10 17:00:11 +0900

    SqlSesson close 적용

--- premierLeague/src/premier/dao/PlayerDao.java
+++ premierLeague/src/premier/dao/PlayerDao.java
@@ -1,27 +1,58 @@
 package premier.dao;
 
+import java.io.Closeable;
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
 import premier.bean.PlayerBean;
 import premier.util.SqlSessionFactoryManager;
+
 public class PlayerDao {
 	private SqlSessionFactory sqlSessionFactory;
-	
-	public PlayerDao(){
-		sqlSessionFactory=SqlSessionFactoryManager.getSqlSessionFactory();
+
+	public PlayerDao() {
+		sqlSessionFactory = SqlSessionFactoryManager.getSqlSessionFactory();
 	}
-	
+
 	public SqlSessionFactory getFactory() {
 		return sqlSessionFactory;
 	}
 
-	public List<PlayerBean> selectAllPlayer(){
-		return sqlSessionFactory.openSession().selectList("selectAllPlayer");
+	// close
+	private void closeSqlSession(Closeable c) {
+		try {
+			if (c != null)
+				c.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	
-	public PlayerBean selectOnePlayer(String playerNo){
-		return sqlSessionFactory.openSession().selectOne("selectOnePlayer", playerNo);
+
+	public List<PlayerBean> selectAllPlayer() {
+		SqlSession sqlSession = null;
+		try {
+			sqlSession = sqlSessionFactory.openSession();
+			return sqlSession.selectList("selectAllPlayer");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			closeSqlSession(sqlSession);
+		}
+	}
+
+	public PlayerBean selectOnePlayer(String playerNo) {
+		SqlSession sqlSession = null;
+		try {
+			sqlSession = sqlSessionFactory.openSession(); 
+			return sqlSession.selectOne("selectOnePlayer", playerNo);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			closeSqlSession(sqlSession);
+		}
 	}
 }
